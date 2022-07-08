e0bbd851b695e8b7c597d4ec594735855bc4d0e6
cocowidmer <ceciliawidmer1993@gmail.com>
2022-07-08 12:39:28 -0400

    fixed name of palette file

--- packages/web/@types/mui.d.ts
+++ packages/web/@types/mui.d.ts
@@ -1,5 +1,5 @@
 import '@last-rev-marketing-site/components/src/@types/components';
 import '@last-rev-marketing-site/components/src/@types/overrides';
-import '@last-rev-marketing-site/components/src/@types/paletteAndTypography';
+import '@last-rev-marketing-site/components/src/@types/palette';
 import '@last-rev-marketing-site/components/src/@types/props';
 // Extend MUI in the components folder
