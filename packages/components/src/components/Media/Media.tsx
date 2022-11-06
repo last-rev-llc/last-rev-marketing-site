12865b0f3fa7dedeb4cccf60c0ad2d7805bdbc84
Max Techera <maxi.techerag@gmail.com>
2022-11-06 15:37:15 -0800

    Update Media for webm

--- packages/components/src/components/Media/Media.tsx
+++ packages/components/src/components/Media/Media.tsx
@@ -1,3 +1,36 @@
-import Media from '@last-rev/component-library/dist/components/Media';
+import React from 'react';
+import LRMedia, { MediaProps } from '@last-rev/component-library/dist/components/Media';
+import { styled } from '@mui/system';
+import sidekick from '@last-rev/contentful-sidekick-util';
 export type { MediaProps, MediaClassKey, MediaClasses } from '@last-rev/component-library/dist/components/Media';
+
+const isVideo = (src?: string) => src && /mp4|webm/.test(src);
+
+const Media = (props: MediaProps) => {
+  if (isVideo(props?.file?.url)) {
+    return (
+      <VideoRoot
+        {...sidekick(props.sidekickLookup)}
+        preload="auto"
+        data-testid={props.testId || 'Media'}
+        {...(props as any)}
+        autoPlay={true}
+        muted
+        loop
+        sx={{ width: '100%', height: '100%', ...props.sx }}>
+        <source src={props?.file?.url} />
+        Your browser does not support the video tag.
+      </VideoRoot>
+    );
+  }
+  return <LRMedia {...props} />;
+};
+
+const VideoRoot = styled('video', {
+  name: 'Media',
+  slot: 'VideoRoot',
+  shouldForwardProp: (prop) => prop !== 'variant',
+  overridesResolver: (_, styles) => [styles.root]
+})<{ variant?: string }>``;
+
 export default Media;
