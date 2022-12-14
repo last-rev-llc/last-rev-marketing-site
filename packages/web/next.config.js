c93d1a5c9d5675da93a83333c21007c1ba411465
Max Techera <maxi.techerag@gmail.com>
2022-12-14 15:55:44 -0300

    Allow cors on API routes

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -69,6 +69,19 @@ const nextConfig = {
           // Apply these headers to all routes in your application.
           source: '/:path*',
           headers: securityHeaders
+        },
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            }
+          ]
         }
       ];
     }
