05f957e70759ac7861ff0e436331a76ae351ab6d
Max Techera <maxi.techerag@gmail.com>
2023-03-08 11:46:01 -0300

    Add dev URL for client

--- packages/utils/src/client.tsx
+++ packages/utils/src/client.tsx
@@ -6,6 +6,7 @@ let URL =
     ? 'http://localhost:5000/graphql'
     : `${process.env.DEPLOY_URL}${process.env.GRAPHQL_SERVER_URL}` ?? 'http://localhost:3000/api/graphql';
 
+if (process.env.NODE_ENV !== 'production') URL = 'http://localhost:3000/api/graphql';
 const sdk = getSdk(new GraphQLClient(URL));
 
 export default sdk;
