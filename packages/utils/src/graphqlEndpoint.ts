861b35608251985c89e6f4f9c1a59adacfbff621
Jaime Morales <jaime@lastrev.com>
2023-03-17 17:25:51 +0100

    logging for debugging

--- packages/utils/src/graphqlEndpoint.ts
+++ packages/utils/src/graphqlEndpoint.ts
@@ -1,5 +1,9 @@
 // DEPLOY_URL supplied by netlify, VERCEL_URL supplied by vercel
+
 const deployUrl = process.env.DEPLOY_URL || process.env.VERCEL_URL;
+
+console.log('envs in gql endpoint', process.env.DEPLOY_URL, process.env.STAGE);
+
 const graphqlEndpoint =
   process.env.STAGE === 'build' || !deployUrl ? 'http://localhost:5000/graphql' : `${deployUrl}/api/graphql`;
 
