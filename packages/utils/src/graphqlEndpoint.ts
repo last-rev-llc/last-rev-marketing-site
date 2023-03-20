09aa5fd99ae07c5d2ffe7bdb58fef4cdce3e5c40
Jaime Morales <jaime@lastrev.com>
2023-03-20 16:21:21 +0100

    removed comments

--- packages/utils/src/graphqlEndpoint.ts
+++ packages/utils/src/graphqlEndpoint.ts
@@ -2,8 +2,6 @@
 
 const deployUrl = process.env.DEPLOY_URL || process.env.VERCEL_URL;
 
-console.log('envs in gql endpoint', process.env.DEPLOY_URL, process.env.STAGE);
-
 const graphqlEndpoint =
   process.env.STAGE === 'build' || !deployUrl ? 'http://localhost:5000/graphql' : `${deployUrl}/api/graphql`;
 
