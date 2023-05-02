0c37c5a9cf9390588c00ad58e4dcc976dda9d1e1
Cameron Taylor <cameron@lastrev.com>
2023-05-02 17:40:22 -0400

    Updating Build Scripts with "es -- "

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,16 +4,16 @@ source .env || echo "No .env file found"
 set +a
 function cleanup() {
     rv=$?
-    yarn gql:pm2:kill
+    es -- yarn gql:pm2:kill
     exit $rv
 }
 
 trap "cleanup" EXIT
 
 echo "Starting develop server..."
-yarn propagate:env
+es -- yarn propagate:env
 if [[ "${GRAPHQL_RUNNER_STRATEGY}" == "fs" ]] || [[ -z "${GRAPHQL_RUNNER_STRATEGY}" ]]; then
     echo "Syncing CMS data..."
-    turbo run sync:cms
+    es -- turbo run sync:cms
 fi
-turbo run dev --output-logs=new-only
+es -- turbo run dev --output-logs=new-only
