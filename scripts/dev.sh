f24ab9b4e5c4da8e8452babc7f0171ad9997ce3b
Cameron Taylor <cameron@lastrev.com>
2023-05-10 15:11:34 -0400

    Starting over...

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -a
+source .env || echo "No .env file found"
+set +a
 function cleanup() {
     rv=$?
     yarn gql:pm2:kill
@@ -8,6 +11,7 @@ function cleanup() {
 trap "cleanup" EXIT
 
 echo "Starting develop server..."
+yarn propagate:env
 if [[ "${GRAPHQL_RUNNER_STRATEGY}" == "fs" ]] || [[ -z "${GRAPHQL_RUNNER_STRATEGY}" ]]; then
     echo "Syncing CMS data..."
     turbo run sync:cms
