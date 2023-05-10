b3a90b5dfbc359f4b84b155bb67b021bd4cf2b5c
Cameron Taylor <cameron@lastrev.com>
2023-05-10 14:04:54 -0400

    Simpler Modification for Envkey Integration

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-set -a
-source .env || echo "No .env file found"
-set +a
+yarn installEnvkey
+
 function cleanup() {
     rv=$?
-    bash "$PWD/scripts/post_build.sh"
+    bash "es -w -- $PWD/scripts/post_build.sh"
     if [[ "$rv" != "0" ]]; then
         echo "Build failed."
         # if node env is buprodild do this
         if [[ "$NODE_ENV" == "prod" ]]; then
-            yarn pm2 logs --nostream --lines=1000
+            es -w -- yarn pm2 logs --nostream --lines=1000
         fi
         
     fi
@@ -18,10 +17,8 @@ function cleanup() {
 
 trap "cleanup" EXIT
 
-yarn propagate:env
-
-bash "$PWD/scripts/pre_build.sh"
+bash "es -w -- $PWD/scripts/pre_build.sh"
 
 # Run build and cleanup pm2 if it fails
 echo "Building..."
-STAGE=build yarn turbo:build --output-logs=new-only $1
+STAGE=build es -w -- yarn turbo:build --output-logs=new-only $1
