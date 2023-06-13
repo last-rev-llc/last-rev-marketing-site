cb28b36f87200f622a640bd6f80fb168991ad894
Cameron Taylor <50385537+ct3685@users.noreply.github.com>
2023-06-13 11:03:47 -0400

    Revert " Integrating Envkey - PROJECT-525 (#73)"
    This reverts commit 45aabdb1e54278091a1d851a57efd3c54bd7fbc3.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+set -a
+source .env || echo "No .env file found"
+set +a
 function cleanup() {
     rv=$?
     yarn gql:pm2:kill
@@ -9,7 +11,7 @@ function cleanup() {
 trap "cleanup" EXIT
 
 echo "Starting develop server..."
-
+yarn propagate:env
 if [[ "${GRAPHQL_RUNNER_STRATEGY}" == "fs" ]] || [[ -z "${GRAPHQL_RUNNER_STRATEGY}" ]]; then
     echo "Syncing CMS data..."
     turbo run sync:cms
