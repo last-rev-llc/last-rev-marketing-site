9af34ee82478240ddc0e70fe6f6b1461c0b4b893
Max Techera <maxi.techerag@gmail.com>
2023-08-10 15:17:58 -0300

    HOTFIX: Add missing folder

---
+++ packages/components/src/components/Form/Form.fragment.graphql
@@ -0,0 +1,12 @@
+fragment Form_BaseFragment on Form {
+  ...Content_BaseFragment
+  variant
+  hubspotPortalId
+  hubspotFormId
+  headerText {
+    ...RichText_BaseFragment
+  }
+  successText {
+    ...RichText_BaseFragment
+  }
+}
