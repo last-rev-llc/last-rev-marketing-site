462a587d4e0337051fc77c62284e4ac3d40b23fe
Adam Harris <adam@lastrev.com>
2024-02-02 08:46:47 -0800

    Update Card.tsx

--- packages/components/src/components/Card/Card.tsx
+++ packages/components/src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+// Test
 import Card from '@last-rev/component-library/dist/components/Card';
 export type { CardProps, CardClassKey, CardClasses } from '@last-rev/component-library/dist/components/Card';
 export default Card;
