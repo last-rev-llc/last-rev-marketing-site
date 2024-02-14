a88c03456132a43c1c132701f9e7223a9cada8ff
Cameron Taylor <cameron@lastrev.com>
2024-02-14 10:36:40 -0500

    Update GraphQL endpoint URLs

--- packages/utils/src/graphqlEndpoint.ts
+++ packages/utils/src/graphqlEndpoint.ts
@@ -3,7 +3,7 @@ const deployUrl = process.env.DEPLOY_URL ?? (process.env.VERCEL_URL ? `https://$
 
 export const graphqlEndpoint =
   process.env.CI || process.env.NEXT_PHASE === 'phase-production-build' || process.env.STAGE === 'build' || !deployUrl
-    ? 'http://localhost:8888/api/graphql'
+    ? 'http://localhost:8888/graphql'
     : `${deployUrl}/api/graphql`;
 
 export default graphqlEndpoint;
