fc1241f2cdbc093c9581416423ef8b3b9575c49d
Adam Harris <adam@lastrev.com>
2024-05-29 09:49:17 -0700

    Added chatbot

---
+++ packages/components/src/components/Chat/Chat.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import { BubbleChat } from 'aai-embed-react';
+
+export interface ChatProps {
+  settings: {
+    chatflowid: string;
+    apiHost: string;
+    other: any;
+    theme?: any;
+  };
+}
+
+function Chat({ settings }: ChatProps) {
+  const { chatflowid, apiHost, other } = settings;
+
+  if (!chatflowid || !apiHost) return null;
+
+  return (
+    <Root>
+      <BubbleChat chatflowid={chatflowid} apiHost={apiHost} {...other} />
+    </Root>
+  );
+}
+
+const Root = styled(Box, {
+  name: 'Chat',
+  slot: 'Root',
+  overridesResolver: (_, styles) => [styles.root]
+})``;
+
+export default Chat;
+3;
