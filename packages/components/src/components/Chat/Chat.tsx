b4801c56e8a8e6bd7c6792ef8ae8506d986267f1
Adam Harris <adam@lastrev.com>
2024-12-06 13:32:55 -0800

    add bubblechat back

--- packages/components/src/components/Chat/Chat.tsx
+++ packages/components/src/components/Chat/Chat.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/material';
 import Box from '@mui/material/Box';
+import { BubbleChat } from 'aai-embed-react';
 
 export interface ChatProps {
   settings: {
